a20bbffdb10912ce785d5c8c4f534898974bc996
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-09 21:35:43 +0700

    Add port definition from environment props 	modified:   src/main/java/com/github/thecoolersuptelov/limitsservice/controllers/Exchange.java

--- src/main/java/com/github/thecoolersuptelov/limitsservice/controllers/Exchange.java
+++ src/main/java/com/github/thecoolersuptelov/limitsservice/controllers/Exchange.java
@@ -1,6 +1,7 @@
 package com.github.thecoolersuptelov.limitsservice.controllers;
 
 import com.github.thecoolersuptelov.limitsservice.Domain.CurrencyExchange;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -8,8 +9,14 @@ import java.util.UUID;
 @RestController
 @RequestMapping(value = "/currency-exchange/")
 public class Exchange {
+    private final Environment environment;
+
+    public Exchange(Environment environment) {
+        this.environment = environment;
+    }
+
     @GetMapping(value = "from/{fromCurrency}/to/{toCurrency}")
     public CurrencyExchange retriveExchangeValue(@PathVariable String fromCurrency, @PathVariable String toCurrency) {
-        return new CurrencyExchange(UUID.randomUUID(), fromCurrency, toCurrency, 100l,"osos");
+        return new CurrencyExchange(UUID.randomUUID(), fromCurrency, toCurrency, 100l, environment.getProperty("local.server.port"));
     }
 }
