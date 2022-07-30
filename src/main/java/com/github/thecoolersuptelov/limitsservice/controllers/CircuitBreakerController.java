d92ce687733941fdba4e0abfea35f65c743e0cf5
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-30 15:06:57 +0700

    Shortner deployment file 	modified:   deployment.yaml 	modified:   src/main/java/com/github/thecoolersuptelov/limitsservice/Domain/CurrencyExchange.java 	modified:   src/main/java/com/github/thecoolersuptelov/limitsservice/controllers/CircuitBreakerController.java

--- src/main/java/com/github/thecoolersuptelov/limitsservice/controllers/CircuitBreakerController.java
+++ src/main/java/com/github/thecoolersuptelov/limitsservice/controllers/CircuitBreakerController.java
@@ -2,7 +2,6 @@ package com.github.thecoolersuptelov.limitsservice.controllers;
 
 import com.github.thecoolersuptelov.limitsservice.Domain.Limits;
 import com.github.thecoolersuptelov.limitsservice.configuration.Configuration;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,19 +13,20 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CircuitBreakerController {
 
-    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @Autowired
     private Configuration configuration;
 
     @GetMapping("/sample-api")
     @Retry(name = "sample-api", fallbackMethod = "fallbackForSampleApi")
 //    @CircuitBreaker(name = "default", fallbackMethod = "fallbackForSampleApi")
-    public String sampleApi(){
+    public String sampleApi() {
         logger.info("Api call received");
         var someFailedRequest = new RestTemplate().getForEntity("http://localhost:9299/failedRequest", String.class);
         return someFailedRequest.getBody();
     }
-    public String fallbackForSampleApi(Throwable e){
+
+    public String fallbackForSampleApi(Throwable e) {
         return new Limits(configuration.getMinimum(), configuration.getMaximum()).toString();
     }
 }
